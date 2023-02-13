216e722f7613e7dc9fc55bd52ecf26356e063a3a
Mkhmialeuski <m.khmialeuski@gmail.com>
2023-02-13 22:01:39 +0300

    next elements

--- the next element is larger than the previous one.py
+++ the next element is larger than the previous one.py
@@ -2,6 +2,6 @@ n = int(input())
 l = []
 for i in range(n):
     l.append(int(input()))
-for i in range(len(l)-1):
+for i in range(len(l) - 1):
     if l[i] < l[i + 1]:
-        print(l[i +1])
+        print(l[i + 1])
