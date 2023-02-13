72bd43c00700b5e08a0cfa7acd6268a2ba1385ee
Mkhmialeuski <m.khmialeuski@gmail.com>
2023-02-13 22:01:25 +0300

    elements

--- 2 elements next to the same sign.py
+++ 2 elements next to the same sign.py
@@ -2,9 +2,9 @@ n = int(input())
 l = []
 for i in range(n):
     l.append(int(input()))
-for i in range(len(l)-1):
-    if l[i] < 0 and  l[i + 1] < 0:
-        print(l[i],l[i +1])
+for i in range(len(l) - 1):
+    if l[i] < 0 and l[i + 1] < 0:
+        print(l[i], l[i + 1])
         break
     if l[i] > 0 and l[i + 1] > 0:
         print(l[i], l[i + 1])
