9a30cdb17c192416aa0433ffe9b27dd0d733eaa6
Mkhmialeuski <m.khmialeuski@gmail.com>
2023-02-14 20:39:17 +0300

    done

--- local maximum.py
+++ local maximum.py
@@ -1,9 +1,12 @@
 n = int(input())
 l = []
+count = 0
 for i in range(n):
     l.append(int(input()))
 for i in range(len(l) - 1):
     if i == 0:
         continue
-    elif l[i - 1] < l[i] > l[i + 1] < 0:
+    elif l[i - 1] < l[i] > l[i + 1]:
         print(l[i])
+        count += 1
+print(count)
