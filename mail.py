c58914276d97aecebb0f38304b5e59905540a6ae
Mkhmialeuski <m.khmialeuski@gmail.com>
2023-03-05 20:36:01 +0300

    email

---
+++ mail.py
@@ -0,0 +1,11 @@
+emails = {'mgu.edu': ['andrei_serov', 'alexander_pushkin', 'elena_belova', 'kirill_stepanov'],
+       'gmail.com': ['alena.semyonova', 'ivan.polekhin', 'marina_abrabova'],
+       'msu.edu': ['sergei.zharkov', 'julia_lyubimova', 'vitaliy.smirnoff'],
+       'yandex.ru': ['ekaterina_ivanova', 'glebova_nastya'],
+       'harvard.edu': ['john.doe', 'mark.zuckerberg', 'helen_hunt'],
+       'mail.ru': ['roman.kolosov', 'ilya_gromov', 'masha.yashkina']}
+s = []
+for k,v in emails.items():
+    for i in v:
+        s.append(i + k)
+print('\n'.join(sorted(s)))
